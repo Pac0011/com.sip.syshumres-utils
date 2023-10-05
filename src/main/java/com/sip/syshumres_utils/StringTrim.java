1201132a6a0386728108f6ba2fad3025cc9f23ea
Pac0011 <prong01@gmail.com>
2023-10-05 15:45:50 -0600

    Ajuste

--- src/main/java/com/sip/syshumres_utils/StringTrim.java
+++ src/main/java/com/sip/syshumres_utils/StringTrim.java
@@ -13,9 +13,9 @@ public class StringTrim {
 	public static String trimAndRemoveDiacriticalMarks(String str) {
 	    //"ÀÂÃÄÅÆÇÈÊËÌÎÏÐÑÒÔÕÖØÙÛÜÝßàâãäåæçèêëìîïðñòôõöøùûüýÿ";
 		if (str != null) {
-			//.replaceAll("[^\\p{ASCII}]", "")
-			//.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "")
-			String newStr = Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+	        // Eliminar marcas diacríticas, excepto acentos y "ñ"
+			String newStr = Normalizer.normalize(str, Normalizer.Form.NFD)
+					.replaceAll("[^\\\\p{InCombiningDiacriticalMarks}&&[^\\\\u0301\\\\u0300]]", "");
 			return newStr.strip();
 		}
 		return str;
